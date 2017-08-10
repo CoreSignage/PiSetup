47c1bc028fc0e823a72adf155005eb8d9b4817b4
CoreSignage <coredialsignage@gmail.com>
2017-08-10 18:00:15 +0000

    Stopped screensaver

--- fullscreen.sh
+++ fullscreen.sh
@@ -2,3 +2,7 @@ sleep 15s;
 sudo -u pi epiphany-browser -a --profile ~/.config display.coredial.net/catalog/image --display=:0 &
 sleep 20s;
 xte "key F11" -x:0
+sleep 10s;
+@xset s off
+@xset -dpms
+@xset s noblank
